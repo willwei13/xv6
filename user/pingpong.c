cf42ae1ff4624001a67baaa88e9752bfbd714aef
willwei13 <101318152+willwei13@users.noreply.github.com>
2023-12-25 18:18:01 +0800

    Add files via upload

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,29 +2,51 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[]){
-    int p2c[2],c2p[2];
+#define N 5
+char buf[N];
 
-    if(pipe(p2c)==-1||pipe(c2p)==-1){
-        printf("pingpong: create pipe error");
-        exit(1);
-    }
-    int pid=fork();
-    char buf[64];
-    if(pid == 0){ //child 
-        read(p2c[0], buf, 4);
-        printf("%d: received %s\n", getpid(), buf);
-        write(c2p[1], "ping", strlen("ping"));
-    }else{
-        // parent
-        write(p2c[1], "pong", strlen("pang"));
-        read(c2p[0], buf, 4);
-        printf("%d: received %s\n", getpid(), buf);
-    }
-    close(p2c[0]);
-    close(p2c[1]);
-    close(c2p[0]);
-    close(c2p[1]);
-    exit(0);
+void
+pong(int *parent_to_child, int *child_to_parent) {
+  if (read(parent_to_child[0], buf, N) < 0) {
+    printf("read failed\n");
+  }
+  printf("%d: received %s\n", getpid(), buf);
+  if (write(child_to_parent[1], "pong", 4) != 4) {
+    printf("write failed\n");
+  }
+}
+
+void
+ping(int *parent_to_child, int *child_to_parent) {
+  
+  if (write(parent_to_child[1], "ping", 4) != 4) {
+    printf("write failed\n");
+  }
+  if (read(child_to_parent[0], buf, N) < 0) {
+    printf("read failed\n");
+  }
+  printf("%d: received %s\n", getpid(), buf);
+}
+
+int
+main(int argc, char *argv[])
+{
+  int parent_to_child[2];
+  int child_to_parent[2];
+
+  int pid;
+
+  if (pipe(parent_to_child) < 0 || pipe(child_to_parent) < 0) {
+    printf("pipe failed\n");
+  }
+  if ((pid = fork()) < 0) {
+    printf("fork failed\n");
+  }
+  if (pid == 0) {
+    pong(parent_to_child, child_to_parent);
+  } else {
+    ping(parent_to_child, child_to_parent);
+  }
+  
+  exit(0);
 }
