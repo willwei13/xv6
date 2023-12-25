45a2a3385ffe2dac34e8252bc2986ceaef009a6e
willwei13 <101318152+willwei13@users.noreply.github.com>
2023-12-25 18:28:31 +0800

    Add files via upload

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -1,8 +1,8 @@
 #include "types.h"
 #include "riscv.h"
+#include "param.h"
 #include "defs.h"
 #include "date.h"
-#include "param.h"
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
@@ -46,6 +46,7 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
+  
   addr = myproc()->sz;
   if(growproc(n) < 0)
     return -1;
@@ -57,6 +58,7 @@ sys_sleep(void)
 {
   int n;
   uint ticks0;
+
 
   if(argint(0, &n) < 0)
     return -1;
@@ -72,6 +74,57 @@ sys_sleep(void)
   release(&tickslock);
   return 0;
 }
+
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{    
+  // lab pgtbl: your code here.
+  int num_check;
+  uint64 va;
+  uint64 dstva;
+  pagetable_t pagetable = myproc()->pagetable;
+  
+  uint64 check_va;
+  pte_t *pte;
+  uint32 buf = 0; 
+  
+  if(argaddr(0, &va) < 0){
+    return -1;
+  }
+  if(argint(1, &num_check) < 0){
+    return -1;
+  }
+  if(argaddr(2, &dstva) < 0){
+    return -1;
+  }
+
+  for(int i = 0; i < MAXSCAN && i < num_check; i++){
+    check_va = va + i * PGSIZE; 
+    pte = walk(pagetable, check_va, 0);
+    if(pte == 0){
+      return -1;
+    }
+    if((*pte & PTE_V) == 0){
+      return -1;
+    }
+    if((*pte & PTE_U) == 0){
+      return -1;
+    }
+    if(*pte & PTE_A){
+      *pte = *pte & ~PTE_A;
+      buf |= (1 << i);  
+    }
+  }
+  
+  if(copyout(pagetable, dstva, (char *)&buf, sizeof(buf)) < 0){
+    return -1;
+  }
+
+  return 0;
+}
+#endif
 
 uint64
 sys_kill(void)
